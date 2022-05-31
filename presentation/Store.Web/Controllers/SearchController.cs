ec62d8d95f670e6ea5ef1668f8fcc7b4b160e6cd
Roman <romanshepa@gmail.com>
2022-05-31 16:10:34 +0300

    Add Razor view by clicking right mouse button on View(18 str) in SearchController class.

--- presentation/Store.Web/Controllers/SearchController.cs
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -4,9 +4,18 @@ namespace Store.Web.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult Index()
+        private readonly IBookRepository bookRepository;
+
+        public SearchController(IBookRepository bookRepository)
         {
-            return View();
+            this.bookRepository = bookRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var books = bookRepository.GetAllByTitle(query);
+            
+            return View(books);
         }
     }
 }
