0772efd46a31379e430b66b4198c2096c2fe3357
Roman <romanshepa@gmail.com>
2022-05-31 15:40:19 +0300

    Add class "BookRepository" to Store.Memory

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,7 +1,19 @@
-﻿namespace Store.Memory
-{
-    public class BookRepository
-    {
+﻿using System.Linq;
 
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private  readonly Book[] _books = new[]
+        {
+            new Book(1, ""),
+            new Book(2, ""),
+            new Book(3, "")
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return _books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
     }
 }
