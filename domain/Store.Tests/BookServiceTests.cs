b533c34d690fe0d2cca289897078fff03736011c
Roman <romanshepa@gmail.com>
2022-06-01 15:55:14 +0300

    Add BookServiceTests class

---
+++ domain/Store.Tests/BookServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Tests
+{
+    public class BookServiceTests
+    {
+        [Fact]
+
+        public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
+        {
+            var bookRepositoryStub = new Mock<IBookRepository>();
+
+            bookRepositoryStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>())).Returns(new[] { new Book(1, "", "", "")});
+            bookRepositoryStub.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>())).Returns(new[] { new Book(2, "", "", "") });
+
+        }
+    }
+}
